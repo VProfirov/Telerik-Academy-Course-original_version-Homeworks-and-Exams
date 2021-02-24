b34a8712dc0e4e2fc621f5a9d5f949081f25fcfc
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-02-24 22:29:48 +0200

    wip

---
+++ 01.Fundamentals/_03_COnsole-Input-Output/.vscode/vscode_project_to_launcher.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+PROJECT_LOCATION=$(PWD)
+
+# PSEUDO CODE SOLUTION
+#cd $PROJECT_LOCATION; cd ../;
+# for dir_name in PWD
+    #if dir_name !=  '.*
+    #PROJECT_NAMES=[].ADD(dir_Name)
+
+# cd .vscode
+# cat > launch.json #REWRITE THE ENTIRE (no-append)
+# for project in PROJECT_NAMES
+    # launch.json-project pattern >> for all projects
