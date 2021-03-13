e00a640ef8c82f2bc0f2e69052ec321758e0f9e5
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-13 23:14:43 +0200

    000

--- 01.Fundamentals/_03_Operators-and-Expressions/_01_OddOrEven/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_01_OddOrEven/Program.cs
@@ -6,7 +6,7 @@ namespace _01_OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Intersting!");
         }
     }
 }
