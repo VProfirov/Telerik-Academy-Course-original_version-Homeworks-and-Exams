b7af566b2c06e2ad80954902441fdc2bba7f5ef2
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-14 15:36:49 +0200

    some work done on the tasks

--- 01.Fundamentals/_03_Operators-and-Expressions/_01_OddOrEven/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_01_OddOrEven/Program.cs
@@ -6,7 +6,13 @@ namespace _01_OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Intersting!");
+            Console.WriteLine($"Enter the number you want to check if its ODD or EVEN: ");
+            var userInput = Console.ReadLine();
+            var userInput_VerificationMSG = string.Empty;
+            int? userNumber = null;
+            var userIpnut_isValid = int.TryParse(userInput,out userNumber?);
+
+            // TODO: Validation confirmation and msg_out -> do it parallel in .py; .js; .go
         }
     }
 }
