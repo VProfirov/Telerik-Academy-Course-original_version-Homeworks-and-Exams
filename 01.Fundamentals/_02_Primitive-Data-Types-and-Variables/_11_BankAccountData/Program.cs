4e6c7b9c981fc069d254fd48e57fe50454e12149
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-17 01:25:42 +0200

    will use hack-ed* solution to debugging to save time
    Will have to write script and then service and a pull request for the Omnisharp for "Track Multiple Projects in Solution"

--- 01.Fundamentals/_02_Primitive-Data-Types-and-Variables/_11_BankAccountData/Program.cs
+++ 01.Fundamentals/_02_Primitive-Data-Types-and-Variables/_11_BankAccountData/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace _11_BankAccountData
 {
     class Program
@@ -13,10 +13,25 @@ namespace _11_BankAccountData
             var currency = "$";
             var bankName = "HSBC";
             var IBAN = "BG00001SF2030JS30";
-            var creditCard_1 = "SomeCardID";
-            var creditCard_2 = "SomeCardID";
-            var creditCard_3 = "SomeCardID";
-
+            var creditCard_1 = "SomeCardID_1";
+            var creditCard_2 = "SomeCardID_2";
+            var creditCard_3 = "SomeCardID_3";
+            
+            var list_users_BankAccountData = new List<string>();
+            list_users_BankAccountData.Add(firstName);
+            list_users_BankAccountData.Add(midleName);
+            list_users_BankAccountData.Add(lastName);
+            list_users_BankAccountData.Add(balanceInDollars.ToString());
+            list_users_BankAccountData.Add(currency);
+            list_users_BankAccountData.Add(bankName);
+            list_users_BankAccountData.Add(IBAN);
+            list_users_BankAccountData.Add(creditCard_1);
+            list_users_BankAccountData.Add(creditCard_2);
+            list_users_BankAccountData.Add(creditCard_3);
+            foreach (var data in list_users_BankAccountData)
+            {
+               Console.WriteLine($"User data: {data}");
+            }
         }
     }
 }
