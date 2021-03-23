045f0fa3a662e1cb9ce6effc9bd832e30cacf41e
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-23 23:26:38 +0200

    no-comment vscode and omnisharp bugs

---
+++ _00_Generic-CSharp-Research_misc/_03_vscode-omnisharp-csharp-debugging-problem/tst_netcoreapp3.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tst_netcoreapp3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 3.1");
+            // Console.WriteLine($"My name is {Console.ReadLine()}");
+
+            string age = Console.ReadLine();
+            Console.WriteLine($"AGE: {age}");
+        }
+    }
+}
