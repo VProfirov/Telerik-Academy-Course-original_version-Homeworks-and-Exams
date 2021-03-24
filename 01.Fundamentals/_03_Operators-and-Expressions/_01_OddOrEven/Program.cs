35a2f2c90395bce3722b3eb0bb30c2cdea370d15
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-24 17:46:01 +0200

    _03_Operators-and-Epressions/_01_OddOrEven => aka 01.03.01 : isDone
    Due to the constant Omnisharp extension/project is malfunctioning for VSCODE, the dev.env will be Rider, until at least the major bugs are fixed.
    
    The Extension for tracking the launch.json and task.json will be suspended until all major bugs in Omnisharp are fixed

--- 01.Fundamentals/_03_Operators-and-Expressions/_01_OddOrEven/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_01_OddOrEven/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Json;
 
 namespace _01_OddOrEven
 {
@@ -6,41 +7,48 @@ namespace _01_OddOrEven
     {
         static void Main(string[] args)
         {
-            // OddOrEven_FullHQC_in_single_method();
-            OddOrEven_LazyEvaluation();
+            OddOrEven_HQC_SHORT_SingleMethod();
+            // OddOrEven_HowNotToDoIt();
         }
 
-        static void OddOrEven_FullHQC_in_single_method()
+        static void OddOrEven_HQC_SHORT_SingleMethod()
         {
-
             Console.WriteLine($"Enter the number you want to check if its ODD or EVEN: ");
             var userInput = Console.ReadLine();
-            var userInput_VerificationMSG = string.Empty;
-            var userIpnut_isValid = int.TryParse(userInput, out int userNumber);
-            var userInput_log_msg = (userIpnut_isValid) ? "int number was provided" : "invalid input(null, float, string, etc...)";
-
+            var isValidUserInput = int.TryParse(userInput, out int numberUserInput);
+            var logUserInputVerificationStatus = (isValidUserInput)? "VALID int type castable input!" : $"INVALID Input ({userInput}). Unable to cast to int.";
+            var isOdd = (numberUserInput & 1) == 1;
+            var result = (isValidUserInput) ? $"The User's number ({numberUserInput}) is {(isOdd ? "ODD" : "EVEN")}" : $"{logUserInputVerificationStatus}";
+            Console.WriteLine(result);
             // TODO: Validation confirmation and msg_out -> do it parallel in .py; .js; .go
         }
 
-        static void OddOrEven_LazyEvaluation()
+        static void OddOrEven_HowNotToDoIt()
         {
             Console.WriteLine($"Insert number to be checked if it is ODD or Even: ");
-            // var userInput_Number;
-            var log_msg_userInput_isValid = "A valid integer number was the user's input";
-            var isValid_userInput = true;
-            var userInput_Number = int.Parse(Console.ReadLine());
-            // if (!int.TryParse(Console.ReadLine(), out int userInput_Number))
-            // {
-            //     log_msg_userInput_isValid = "The input is not a valid int. Instead it might be (string, null, decimal num, etc...)";
-            //     isValid_userInput = false;
-            // }
+            // var numberUserInput;
+            var logUserInput = "A valid integer number was the user's input";
+            var isValidUserInput = true;
+            var userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out int numberUserInput))
+            {
+                logUserInput = "The input is not a valid int.\r\n" +
+                               $"The actual input is ({userInput})\r\n" +
+                               "Instead of the requested type int it might be string, null, decimal num, etc...";
+                isValidUserInput = false;
+            }
 
-            var isODD = (userInput_Number & 1) == 1;
+            var isOdd = (numberUserInput & 1) == 1;
 
-            var std_out = (isValid_userInput)
-                ? $"The user's number({userInput_Number}) is ODD => {isODD}"
-                : "Wrong user input! Check the logs!";
-            Console.WriteLine(std_out);
+            var stdout = (isValidUserInput)
+                ? $"The user's number({numberUserInput}) is {(isOdd? "ODD" : "EVEN")}"
+                : $"Wrong user input! Check the logs!";
+            
+            Console.WriteLine(stdout);
+            if (!isValidUserInput)
+            {
+                Console.WriteLine($"---LOG---\r\n{logUserInput}\r\n---END-LOG---");
+            }
         }
     }
 }
