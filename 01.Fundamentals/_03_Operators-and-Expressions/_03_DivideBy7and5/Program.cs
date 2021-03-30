0d083b0154cd2bc6a49a6916d14b7ee02a223833
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-03-30 17:16:37 +0300

    01.03.03.v02

--- 01.Fundamentals/_03_Operators-and-Expressions/_03_DivideBy7and5/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_03_DivideBy7and5/Program.cs
@@ -7,36 +7,22 @@ namespace _03_DivideBy7and5
     {
         static void Main(string[] args)
         {
-            Manual_Solution_Test();
+            Solution_DivisibilityBy5and7();
         }
 
-        static bool Divided_Solution(int number)
+        static void Solution_DivisibilityBy5and7()
         {
             Console.WriteLine($"Enter integer number to be checked for possible divison of 7 and 5 without remainder: ");
             var userInput = Console.ReadLine();
-            var isValidInput = int.TryParse(userInput, out int num);
-            var logValidityOfUserInput = isValidInput ? $"Valid user input as number : {num}" : "Invalid user input! Can't be as integer number.";
-            return true; //TODO: finish the solution
+            var isValidInput = int.TryParse(userInput, out int number);
+            var logValidityOfUserInput = isValidInput ? $"Valid user input as number : {number}" : "Invalid user input! Can't be parsed as integer number.";
+            var isDivisibleBy5and7 = isValidInput ? CheckDivisibilityBy5and7(number) : false;
+            Console.WriteLine(isValidInput ? $" Is the number({number}) divisible by 5 and 7 ==> {isDivisibleBy5and7}" : logValidityOfUserInput);
         }
 
-        static void Manual_Solution_Test()
+        static bool CheckDivisibilityBy5and7(int number)
         {
-            var div_by_35 = new List<double>();
-            var isDiv_by_35_noR = new List<bool>();
-            // var div_by_5_and_7 = new List<double> ();
-            var div_by_5 = new List<double>();
-            var div_by_7 = new List<double>();
-            var isDiv_by_5_and_7_noR = new List<bool>();
-            for (int i = 0; i < 100; ++i)
-            {
-                div_by_35.Add(i / 35);
-                isDiv_by_35_noR.Add(i % 35 == 0);
-
-                div_by_5.Add(i / 5);
-                div_by_7.Add(i / 7);
-                isDiv_by_5_and_7_noR.Add((i % 5 == 0) && (i % 7 == 0));
-            }
-            Console.WriteLine($"Check the debugger {Console.ReadLine()}");
+            return (number % 5 == 0) && (number % 7 == 0);
         }
     }
 }
--- 01.Fundamentals/_03_Operators-and-Expressions/_04_Rectangles/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_04_Rectangles/Program.cs
@@ -8,5 +8,22 @@ namespace _04_Rectangles
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void Rectangles_Solution()
+        {
+
+        }
+
+        class Rectangle
+        {
+            public Rectangle(int width, int height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+
+            public int Width { get; set; }
+            public int Height { get; set; }
+        }
     }
 }
