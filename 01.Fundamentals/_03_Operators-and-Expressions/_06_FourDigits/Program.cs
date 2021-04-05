c166948e13a762e874fe8973f38720be56e5ebf0
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-04-05 19:30:35 +0300

    01.03.06.WIP

--- 01.Fundamentals/_03_Operators-and-Expressions/_06_FourDigits/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_06_FourDigits/Program.cs
@@ -6,7 +6,50 @@ namespace _06_FourDigits
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // TASK: Given an integer number number:
+            // 1. print the digits of the number as a sum
+            // 2. print the digits of the number in reversed order
+            // 3. print abcd as {d}abc
+            // 4. print abcd as a{c}{b}d
+
+            FourDigits_Solution_Hardcoded_Numeric(9876);
+        }
+
+        // The simplest solution to the current problem =>
+        // 1. No input checks/validation
+        // 2. No logging of anysorts
+        // 3. No fault tolerance
+        static void FourDigits_Solution_Hardcoded_Numeric(int number)
+        {
+            FourDigits_Solution_Hardcoded_Numeric_T1(number);
+            FourDigits_Solution_Hardcoded_Numeric_T2(number);
+            FourDigits_Solution_Hardcoded_Numeric_T3(number);
+            FourDigits_Solution_Hardcoded_Numeric_T4(number);
+        }
+        static void FourDigits_Solution_Hardcoded_Numeric_T1(int number)
+        {
+            var sum = 0;
+            var counter = 0;
+            do
+            {
+                sum += number % 10;
+                number /= 10;
+            } while (++counter < 4);
+            Console.WriteLine($"SUM: {sum}");
+        }
+
+
+        static void FourDigits_Solution_Hardcoded_Numeric_T2(int number)
+        {
+
+        }
+        static void FourDigits_Solution_Hardcoded_Numeric_T3(int number)
+        {
+
+        }
+        static void FourDigits_Solution_Hardcoded_Numeric_T4(int number)
+        {
+
         }
     }
 }
