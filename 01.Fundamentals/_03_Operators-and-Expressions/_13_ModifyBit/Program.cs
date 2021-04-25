7489f0a69ae229cf54ca055a57f90ee19c8aaca3
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-04-25 23:19:05 +0300

    1.03.13v90
    TODO: diff check + release version

--- 01.Fundamentals/_03_Operators-and-Expressions/_13_ModifyBit/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_13_ModifyBit/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _13_ModifyBit
 {
@@ -6,7 +7,31 @@ namespace _13_ModifyBit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ModifyBit_Solution();
+        }
+
+        static void ModifyBit_Solution()
+        {
+            Console.Write("Number: ");
+            var isValidInputNumber = int.TryParse(Console.ReadLine(), out int number);
+            Console.Write("Position: ");
+            var isValidInputPosition = int.TryParse(Console.ReadLine(), out int position);
+            Console.Write("Bit value: ");
+            var isValidInputBitValue = int.TryParse(Console.ReadLine(), out int bitValue);
+            Console.Write("Expected modified number: ");
+            var isValidInputExpectedModifiedNumber = int.TryParse(Console.ReadLine(), out int expected_modified_number);
+            VisualAid(number, position, bitValue, expected_modified_number);
+        }
+
+        static void VisualAid(int starting_number, int position, int bitValue, int? expected_number)
+        {
+            Console.WriteLine($"{Convert.ToString(starting_number, 2).PadLeft(32, '0')}");
+            // bitValue change is irrelevant for the next XOR as if it is the opposite!
+            // BUG POTENTIAL => the bit to change is 1 and it is required to be changed to 1
+            // FIXME: insert a bit at position difference check!
+            var changed_number = starting_number ^ (1 << position);
+            Console.WriteLine($"{Convert.ToString(changed_number, 2).PadLeft(32, '0')}");
+            Console.WriteLine($"{Convert.ToString(expected_number ?? 0, 2).PadLeft(32, '0')}");
         }
     }
 }
