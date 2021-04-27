1e82771db9be219acd20877ae5fd2e3ccefb3075
Vasil Profirov <Vasil.Profirov@gmail.com>
2021-04-27 13:57:36 +0300

    1.03.14v80%
    Trying to find a short/elegant way to use bit-combing to exchange bit sets

--- 01.Fundamentals/_03_Operators-and-Expressions/_14_BitExchange/Program.cs
+++ 01.Fundamentals/_03_Operators-and-Expressions/_14_BitExchange/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _14_BitExchange
 {
@@ -6,7 +7,61 @@ namespace _14_BitExchange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exchange bits at 3,4,5 with 24,25,26
+            ExchangeBits_Solution();
+        }
+
+        static void ExchangeBits_Solution()
+        {
+            Console.Write($"Number: ");
+            var isValidInputNumber = uint.TryParse(Console.ReadLine(), out uint number);
+            var modifiedNumber = ExchangeBits(number);
+
+            VisualAid(number);
+        }
+
+        static uint ExchangeBits(uint number)
+        {
+            uint bitCome01 = 3;
+            var position01 = 3;
+            uint bitCome02 = 3;
+            var position02 = 24;
+
+            bitCome01 = (number >> position01) & bitCome01;
+            bitCome02 = (number >> position02) & bitCome02;
+
+            // a^=b exchange of the bitcomes
+            uint exchangeMask = bitCome01 ^ bitCome02;
+            bitCome01 ^= exchangeMask;
+            bitCome02 ^= exchangeMask;
+
+            // repositioning and coming
+            number ^= bitCome01 << position01;
+            number ^= bitCome02 << position02;
+
+            return number;
+        }
+
+        static void VisualAid(uint starting_number, uint expected_number = 0)
+        {
+            if(expected_number == 0)
+            {
+                Console.Write($"Expected Number: ");
+                var isValidInputExpectedNumber = uint.TryParse(Console.ReadLine(), out expected_number);
+            }
+
+            var starting_number_bitwise= Convert.ToString(starting_number, 2).PadLeft(64, '0');
+            var starting_number_bitwise_formatted = Regex.Replace(starting_number_bitwise, ".{8}(?!$)", "$0 ");
+            Console.WriteLine($"{starting_number_bitwise_formatted} \t Starting Number({starting_number})");
+
+            var changed_number = ExchangeBits(starting_number);
+            var changed_number_bitwise = Convert.ToString(changed_number, 2).PadLeft(64, '0');
+            var changed_number_bitwise_formatted = Regex.Replace(changed_number_bitwise, ".{8}(?!$)", "$0 ");
+            Console.WriteLine($"{changed_number_bitwise_formatted} \t Changed Number({changed_number})");
+
+            var expected_number_bitwise = Convert.ToString(expected_number, 2).PadLeft(64, '0');
+            var expected_number_bitwise_formatted = Regex.Replace(expected_number_bitwise, ".{8}(?!$)", "$0 ");
+            Console.WriteLine($"{expected_number_bitwise_formatted} \t Expected Number({expected_number})");
         }
     }
 }
